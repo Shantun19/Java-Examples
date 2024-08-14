64a3dd40e562f71c5a10fc0db2beab0e76d3633f
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-08-14 19:22:06 +0530

    Create 62_CompareTwoString.java

---
+++ 62_CompareTwoString.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // Java Program to Compare Strings
+        String style = "Bold";
+        String style2 = "Bold";
+        
+        if(style == style2) System.out.println("Equal");
+        else System.out.println("Not Equal");
+        
+        if(style.equals(style2)) System.out.println("String are Equals");
+        else System.out.println("String are not Equals");
+    }
+}
