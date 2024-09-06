02d68811911fda77f84d3e3e008ec18b872ab94d
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-09-06 14:52:27 +0530

    Create 66_ConvertCharToInt.java

---
+++ 66_ConvertCharToInt.java
@@ -0,0 +1,13 @@
+// Java Program to convert char type variables to int
+class HelloWorld {
+    public static void main(String[] args) {
+        char a = '5';
+        char b = 'c';
+        
+        int a1 = a;
+        int b1 = b;
+        
+        System.out.println(a1); // store the aschii code 
+        System.out.println(b1); // store the aschii oode 
+    }
+}
