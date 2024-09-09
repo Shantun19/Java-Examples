cffafb6b2d04760a5cdd92ce871c12a0d8db414c
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-09-09 20:09:38 +0530

    Create 73_MultiplyTwoBinaryNumber.java

---
+++ 73_MultiplyTwoBinaryNumber.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Sample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first Binary Number !");
+        long binary1 = sc.nextLong();
+        long dec1 = convertBinaryToDecimal(binary1);
+        System.out.println("Enter Second Binary Number !");
+        long binary2 = sc.nextLong();
+        long dec2 = convertBinaryToDecimal(binary2);
+        System.out.println("The product is => " + decimalToBinary(dec1*dec2));
+
+    }
+
+    public static long convertBinaryToDecimal(long num) {
+        int count = 0;
+        long decimal = 0;
+        while(num != 0) {
+            decimal = decimal + (int) ((num % 10) * Math.pow(2, count));
+            count++;
+            num /= 10;
+        }
+
+        return decimal;
+    }
+
+    public static String decimalToBinary(long num) {
+        String ans = "";
+        while(num != 0) {
+            long rem = num % 2;
+            ans = rem + ans;
+            num/=2;
+        }
+
+        return ans;
+    }
+}
