39f2a2274a8446b0220fd27cac198e7cacac68e0
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-09-15 20:50:34 +0530

    Create 90_ASCHIIOfChar.java

---
+++ 90_ASCHIIOfChar.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Sample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        System.out.println("The ASCHII values of " + ch + " is " + (int)ch);
+    }
+}
