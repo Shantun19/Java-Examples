7aff44e76d923791a66aee406e33ab40c359555d
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-10-19 16:49:00 +0530

    Create 98_printSquarePattern.java

---
+++ 98_printSquarePattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        // pattern printing.
+        int r = 5;
+        int c = 5;
+
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
